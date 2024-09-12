902ab55f8dbff651983abd053542edea2de5cbdc
jethomas787 <jethomas.ison@gmail.com>
2024-09-11 21:55:22 -0700

    purchase init

---
+++ Models/ErpSupplierTemp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace ERPMIS331.Models
+{
+    public partial class ErpSupplierTemp
+    {
+        public short SupplierId { get; set; }
+        public string SupplierName { get; set; }
+        public string SupplierAddress { get; set; }
+        public string SupplierEmail { get; set; }
+        public string SupplierPhoneNumber { get; set; }
+    }
+}
